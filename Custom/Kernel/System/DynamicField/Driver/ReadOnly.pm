c5dbe75fcef8b12ca76de8327d0f052627a71d69
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2022-09-25 22:25:01 +0800

    Create ReadOnly.pm

---
+++ Custom/Kernel/System/DynamicField/Driver/ReadOnly.pm
@@ -0,0 +1,208 @@
+# --
+# Copyright (C) 2022 mo-azfar, https://github.com/mo-azfar/OTRS-Znuny-Read-Only-DynamicField-Text
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::System::DynamicField::Driver::ReadOnly;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+
+use base qw(Kernel::System::DynamicField::Driver::BaseText);
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::DynamicFieldValue',
+    'Kernel::System::Main',
+);
+
+=head1 NAME
+
+Kernel::System::DynamicField::Driver::ReadOnly.pm
+
+=head1 DESCRIPTION
+
+DynamicFields Text Driver delegate
+
+=head1 PUBLIC INTERFACE
+
+This module implements the public interface of L<Kernel::System::DynamicField::Backend>.
+Please look there for a detailed reference of the functions.
+
+=cut
+no warnings 'redefine';
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # set field behaviors
+    $Self->{Behaviors} = {
+        'IsACLReducible'               => 0,
+        'IsNotificationEventCondition' => 1,
+        'IsSortable'                   => 1,
+        'IsFiltrable'                  => 0,
+        'IsStatsCondition'             => 1,
+        'IsCustomerInterfaceCapable'   => 1,
+        'IsLikeOperatorCapable'        => 1,
+    };
+
+    # get the Dynamic Field Backend custom extensions
+    my $DynamicFieldDriverExtensions
+        = $Kernel::OM->Get('Kernel::Config')->Get('DynamicFields::Extension::Driver::Text');
+
+    EXTENSION:
+    for my $ExtensionKey ( sort keys %{$DynamicFieldDriverExtensions} ) {
+
+        # skip invalid extensions
+        next EXTENSION if !IsHashRefWithData( $DynamicFieldDriverExtensions->{$ExtensionKey} );
+
+        # create a extension config shortcut
+        my $Extension = $DynamicFieldDriverExtensions->{$ExtensionKey};
+
+        # check if extension has a new module
+        if ( $Extension->{Module} ) {
+
+            # check if module can be loaded
+            if (
+                !$Kernel::OM->Get('Kernel::System::Main')->RequireBaseClass( $Extension->{Module} )
+                )
+            {
+                die "Can't load dynamic fields backend module"
+                    . " $Extension->{Module}! $@";
+            }
+        }
+
+        # check if extension contains more behaviors
+        if ( IsHashRefWithData( $Extension->{Behaviors} ) ) {
+
+            %{ $Self->{Behaviors} } = (
+                %{ $Self->{Behaviors} },
+                %{ $Extension->{Behaviors} }
+            );
+        }
+    }
+
+    return $Self;
+}
+
+sub EditFieldRender {
+    my ( $Self, %Param ) = @_;
+
+    # take config from field config
+    my $FieldConfig = $Param{DynamicFieldConfig}->{Config};
+    my $FieldName   = 'DynamicField_' . $Param{DynamicFieldConfig}->{Name};
+    my $FieldLabel  = $Param{DynamicFieldConfig}->{Label};
+
+    my $Value = '';
+
+    # set the field value or default
+    if ( $Param{UseDefaultValue} ) {
+        $Value = ( defined $FieldConfig->{DefaultValue} ? $FieldConfig->{DefaultValue} : '' );
+    }
+    $Value = $Param{Value} // $Value;
+
+    # extract the dynamic field value from the web request
+    my $FieldValue = $Self->EditFieldValueGet(
+        %Param,
+    );
+
+    # set values from ParamObject if present
+    if ( defined $FieldValue ) {
+        $Value = $FieldValue;
+    }
+
+    # check and set class if necessary
+    my $FieldClass = 'DynamicFieldText W50pc';
+    if ( defined $Param{Class} && $Param{Class} ne '' ) {
+        $FieldClass .= ' ' . $Param{Class};
+    }
+	
+    # set field as mandatory
+    if ( $Param{Mandatory} ) {
+        $FieldClass .= ' Validate_Required';
+    }
+
+    # set error css class
+    if ( $Param{ServerError} ) {
+        $FieldClass .= ' ServerError';
+    }
+
+
+    my $ValueEscaped = $Param{LayoutObject}->Ascii2Html(
+        Text => $Value,
+    );
+
+    my $FieldLabelEscaped = $Param{LayoutObject}->Ascii2Html(
+        Text => $FieldLabel,
+    );
+
+    my $HTMLString = <<"EOF";
+<input type="text" class="$FieldClass" id="$FieldName" name="$FieldName" title="$FieldLabelEscaped" value="$ValueEscaped"/>
+EOF
+	
+	if ( $Param{Mandatory} ) {
+        my $DivID = $FieldName . 'Error';
+		
+		my $FieldRequiredMessage = $Param{LayoutObject}->{LanguageObject}->Translate("This field is required.");
+
+        # for client side validation
+        $HTMLString .= <<"EOF";
+<div id="$DivID" class="TooltipErrorMessage">
+    <p>
+        $FieldRequiredMessage
+    </p>
+</div>
+EOF
+    }
+	
+	if ( $Param{ServerError} ) {
+
+        my $ErrorMessage = $Param{ErrorMessage} || 'This field is required.';
+        $ErrorMessage = $Param{LayoutObject}->{LanguageObject}->Translate($ErrorMessage);
+        my $DivID = $FieldName . 'ServerError';
+
+        # for server side validation
+        $HTMLString .= <<"EOF";
+<div id="$DivID" class="TooltipErrorMessage">
+    <p>
+        $ErrorMessage
+    </p>
+</div>
+EOF
+    }
+	
+	# --
+	# ro validation
+	if ( $ValueEscaped ne "" ) {
+		
+    	$HTMLString = <<"EOF";
+<input type="text" class="$FieldClass" id="$FieldName" name="$FieldName" title="$FieldLabelEscaped" value="$ValueEscaped" readonly/>
+EOF
+    }
+	# --
+	
+    # call EditLabelRender on the common Driver
+    my $LabelString = $Self->EditLabelRender(
+        %Param,
+        Mandatory => $Param{Mandatory} || '0',
+        FieldName => $FieldName,
+    );
+
+    my $Data = {
+        Field => $HTMLString,
+        Label => $LabelString,
+    };
+
+    return $Data;
+}
+
+1;
